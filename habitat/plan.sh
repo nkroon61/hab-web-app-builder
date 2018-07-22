5e3bba968040ad2bc6a79238aa0aaf8860a5e2f4
Ned Kroon <nkroon@ford.com>
2018-07-22 10:29:29 -0400

    Use my ImageMagick package

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,5 +1,5 @@
 pkg_name=habitatize
 pkg_origin=ce-aem
 pkg_scaffolding="core/scaffolding-ruby"
-pkg_version="0.2.0"
-pkg_deps=( core/imagemagick )
+pkg_version="0.3.0"
+pkg_deps=( ce-aem/imagemagick )
